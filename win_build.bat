7044960abc77ec1c686d87da3a19758da5ecfc6f
diaosuyu <diaosuyu@sk1999.com>
2025-04-08 18:20:42 +0800

    1. Separate Python source from UEFI folder 2. Sync MSVC source
    Change-Id: I29418c7b8b4828ccee55cb84faed45b5237cb47a
    Signed-off-by: diaosuyu <diaosuyu@sk1999.com>

--- win_build.bat
+++ win_build.bat
@@ -11,7 +11,7 @@ set PYTHON_COMMAND=%WORKSPACE%\Tools\BuildTools\Python\Python313\python.exe
 set IASL_PREFIX=%WORKSPACE%\Tools\BuildTools\ASL\
 set GIT_COMMAND=%WORKSPACE%\Tools\BuildTools\Git\cmd\git.exe
 @REM # Please Set this to your actual path
-set VS_ROOT_PATH=C:\Program Files\Microsoft Visual Studio\2022\Community 
+set VS_ROOT_PATH=%WORKSPACE%\Tools\BuildTools\MSVC\Microsoft Visual Studio\2022\Community 
 
 @REM # Update submodule in root path
 %GIT_COMMAND% submodule update --init
